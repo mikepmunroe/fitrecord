4ce0d39bfb73740b7b7d523592a61effc11ca00d
Mike Munroe <mike@mikemunroe.com>
2014-03-12 07:11:25 -0400

    start building out lifts

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ App = Ember.Application.create({
 });
 
 App.Router.map(function() {
-  this.route('about', { path: 'aboutus' });
+  this.route('about');
+  this.route('usersAndCompanies');
+  this.resource('lifts');
 });
 
 App.IndexRoute = Ember.Route.extend({
@@ -11,3 +13,26 @@ App.IndexRoute = Ember.Route.extend({
     return ['red', 'yellow', 'blue'];
   }
 });
+
+App.LiftsRoute = Ember.Route.extend({
+  model: function() {
+    return App.Lifts;
+  }
+});
+
+App.Lifts = [
+  {
+    date: '10/25/2014',
+    name: 'Back Squat',
+    repScheme: '1',
+    load: '325',
+    notes: ''
+  },
+  {
+    date: '10/26/2014',
+    name: 'Front Squat',
+    repScheme: '1',
+    load: '305',
+    notes: ''
+  }
+];

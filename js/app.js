6c8683a0da67b245bb73a51a5bb9aef2e9598ef3
Mike Munroe <mike@mikemunroe.com>
2014-03-14 14:46:41 -0400

    start implementing ember data

--- js/app.js
+++ js/app.js
@@ -2,38 +2,44 @@ App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
 
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
 App.Router.map(function() {
   this.route('about');
-  this.resource('lifts');
-  this.resource('lift', { path: 'lifts/:id' });
+  this.resource('lifts', function() {
+    this.resource('lift', { path: '/:lift_id' });
+  });
 });
 
 App.LiftsRoute = Ember.Route.extend({
   model: function() {
-    return App.Lifts;
-  }
-});
-App.LiftRoute = Ember.Route.extend({
-  model: function(params) {
-    return App.Lifts.findBy('id', params.id);
+    return this.store.findAll('lift');
   }
 });
 
-App.Lifts = [
+App.Lift = DS.Model.extend({
+  date: DS.attr('date'),
+  name: DS.attr('string'),
+  repScheme: DS.attr('number'),
+  load: DS.attr('number'),
+  notes: DS.attr('string')
+});
+
+App.Lift.FIXTURES = [
   {
-    id: '1',
+    id: 1,
     date: '10/25/2014',
     name: 'Back Squat',
-    repScheme: '1',
-    load: '325',
+    repScheme: 1,
+    load: 325,
     notes: ''
   },
   {
-    id: '2',
+    id: 2,
     date: '10/26/2014',
     name: 'Front Squat',
-    repScheme: '1',
-    load: '305',
+    repScheme: 1,
+    load: 305,
     notes: ''
   }
 ];

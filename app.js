c003c06ba5de1a87679be191bab3967414ed1156
Mike Munroe <mike@mikemunroe.com>
2014-03-22 11:44:45 -0400

    finish implementing node, so homepage renders successfully

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var routes = require('./routes');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -19,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
+
+app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));

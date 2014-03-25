1059fffaf1f27b426e375f45a0f47b7247b5df58
Mike Munroe <mike@mikemunroe.com>
2014-03-25 08:57:53 -0700

    define lift schema

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('./db/db');
 var express = require('express');
 var http = require('http');
 var path = require('path');
---
+++ db/db.js
@@ -0,0 +1,13 @@
+var mongoose = require( 'mongoose' );
+var Schema   = mongoose.Schema;
+
+var Lift = new Schema({
+    name: String,
+    date: { type: Date, default: Date.now },
+    repScheme: Number,
+    load: Number,
+    notes: String
+});
+
+mongoose.model( 'Lift', Lift );
+mongoose.connect( 'mongodb://localhost/fitrecord' );

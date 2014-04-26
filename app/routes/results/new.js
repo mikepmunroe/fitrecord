a6cdf42b2987d34b975c050943f02096abcdc271
Mike Munroe <mike@mikemunroe.com>
2014-04-26 14:17:14 -0400

    finish form to add review

--- app/routes/results/new.js
+++ app/routes/results/new.js
@@ -1,6 +1,13 @@
 var ResultsNewRoute = Ember.Route.extend({
   model: function() {
-    return this.store.createRecord('result');
+    return Ember.RSVP.hash({
+      result: this.store.createRecord('result'),
+      lifts: this.store.findAll('lift')
+    });
+  },
+  setupController: function(controller, model) {
+    controller.set('model', model.result);
+    controller.set('lifts', model.lifts);
   }
 });
 

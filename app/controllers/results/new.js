3c305fd0bdc5ee67c81705806e610de21f6f0e1f
Mike Munroe <mike@mikemunroe.com>
2014-04-28 20:35:38 -0400

    start of handling of dates in form

--- app/controllers/results/new.js
+++ app/controllers/results/new.js
@@ -1,4 +1,11 @@
 var ResultsNewController = Ember.Controller.extend({
+  format: null,
+  date: new Date(),
+  formattedDate: function() {
+    var date = this.get('date'),
+        format = this.get('format');
+    return moment(date).format(format);
+  }.property('format', 'date'),
   actions: {
     createResult: function() {
       var controller = this;
